e3b51adf832ec7b7ae1223cda1ccba7e1b09017b
Nicolo Veneziale <nicoloveneziale@gmail.com>
2025-04-22 07:48:13 +0100

    Removed logs

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -2,19 +2,17 @@ const passport = require("passport");
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const db = require("../db/queries");
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret"; // Use .env in production
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // looks for token in Authorization header: Bearer <token>
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: JWT_SECRET,
 };
 
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
-    console.log("JWT Payload:", jwt_payload);
     try {
       const user = await db.getUser(jwt_payload.id);
-      console.log("Fetched User:", user);
       if (user) {
         return done(null, user);
       } else {
